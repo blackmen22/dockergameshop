445496131a81f4d008ab968bae9e8c9117e32d15
Agnes <agnieszka.izuk.01@gmail.com>
2020-05-15 18:20:34 +0200

    initial backend

---
+++ back/src/app/server.ts
@@ -0,0 +1,47 @@
+import * as express from "express";
+import {
+  userRoute, categoryRoute,
+  productRoute, errorLogRoute,
+  wishlistRoute, cartRoute,
+  orderRoute
+} from "./routes/index";
+import * as bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import { MongoConnect } from "./db/db";
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(compression());
+
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT");
+  next();
+});
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+
+app.use("/user", userRoute);
+app.use("/category", categoryRoute);
+app.use("/product", productRoute);
+app.use("/errorLog", errorLogRoute);
+app.use("/wishlist", wishlistRoute);
+app.use("/cart", cartRoute);
+app.use("/order", orderRoute);
+
+app.listen(process.env.PORT || 3000, () => {
+  MongoConnect.connect().then(res => console.log("DB connected"));
+  console.log("Server running on port 3000");
+});
